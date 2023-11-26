767cf0d6859580d2d2b8ebffa998aa2f05e7739b
onart <59155416+onart@users.noreply.github.com>
2023-11-26 18:05:52 +0900

    Update voronoi.h

--- voronoi/voronoi.h
+++ voronoi/voronoi.h
@@ -1,4 +1,3 @@
-// PUBLIC DOMAIN.
 #include <vector>
 #include <cstdint>
 
@@ -9,12 +8,16 @@ namespace onart{
         inline bool operator<(const vec2& other){return y < other.y;}
     };
 
-    // base board range: [0,1]^2 (left-top side (0,0))
+    // 보드의 범위는 [0,1]^2 입니다. (좌측 상단이 (0,0))
     struct VoronoiDiagram{
         std::vector<vec2> sites; // input
 
         std::vector<vec2> areaV; // intersection (Vertices)
-        std::vector<std::vector<uint32_t>> areaE; // cell (set of Edges)
+        std::vector<std::vector<uint32_t>> areaI; // cell (set of Indices per face, ccw)
+
+        /// @brief calculate voronoi diagram for current sites. no duplicate vertices
         void calculate();
+        /// @brief duplicate vertices so that each faces have very their vertices
+        void makeVertexPerFace();
     };
 }
